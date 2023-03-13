f94a2797de527736a5472ab13db59a0e289e2ff8
Hiromasa Ono <hono@dbcls.rois.ac.jp>
2023-03-14 08:54:50 +0900

    fix download links for svg, ai

--- components/DownloadModal.vue
+++ components/DownloadModal.vue
@@ -151,11 +151,7 @@ export default Vue.extend({
         let link = document.createElement("a");
         link.target = "_blank";
         link.download = name;
-        if (name.slice(-3) === "png") {
-          link.href = `https://dbarchive.biosciencedbc.jp/data/togo-pic/image/${name}`;
-        } else {
-          link.href = `ftp://ftp.biosciencedbc.jp/archive/togo-pic/image/${name}`;
-        }
+        link.href = `https://dbarchive.biosciencedbc.jp/data/togo-pic/image/${name}`;
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
