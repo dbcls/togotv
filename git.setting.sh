720380af8a1b2656a1c21c4641d69ff89529d2a3
suimye <suimye@gmail.com>
2025-10-09 21:05:20 +0900

    initial commit for member update

--- git.setting.sh
+++
@@ -1,7 +0,0 @@
-#git checkout -b fix/update-staff-202510
-git add static/json/ja.json
-git add assets/img/member/suimye.png 
-git add pages/staff.vue 
-git commit -m "ja.json: Member list update (morioka, sakuma, minowa)"
-git push origin fix/update-staff-20251
-
